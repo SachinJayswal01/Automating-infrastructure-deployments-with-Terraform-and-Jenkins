ef9c7f8ce67dfdd6e1c7bbccadf4d33f9f56fc0d
Sachin Jayswal <85438478+SachinJayswal01@users.noreply.github.com>
2025-02-21 11:37:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -79,7 +79,6 @@ resource "aws_instance" "webserver1" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.sub1.id
-  user_data              = base64encode(file("userdata.sh"))
 }
 
 resource "aws_instance" "webserver2" {
@@ -87,5 +86,4 @@ resource "aws_instance" "webserver2" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.sub2.id
-  user_data              = base64encode(file("userdata1.sh"))
 }
