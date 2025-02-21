dc7dfb1142c4d510fd31f4cf1d4b54bbb8264967
Sachin Jayswal <85438478+SachinJayswal01@users.noreply.github.com>
2025-02-21 11:26:55 +0530

    Webserver1

--- main.tf
+++ main.tf
@@ -74,4 +74,10 @@ resource "aws_s3_bucket" "example" {
   bucket = "sachinterraformbucket"
 }
 
-
+resource "aws_instance" "webserver1" {
+  ami                    = "ami-0261755bbcb8c4a84"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+  subnet_id              = aws_subnet.sub1.id
+  user_data              = base64encode(file("userdata.sh"))
+}
